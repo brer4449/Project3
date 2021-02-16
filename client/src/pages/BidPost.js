d5ffb32ebace89d69540b2ac703b8d4af220c5af
brer4449 <58540640+brer4449@users.noreply.github.com>
2021-02-15 21:46:51 -0500

    items now displaying properly

--- client/src/pages/BidPost.js
+++ client/src/pages/BidPost.js
@@ -167,9 +167,96 @@ class Bid extends Component {
               </form>
               <br></br>
               <br></br>
-              {/* FIGURE THIS OUT, NOT CURRENTLY DISPLAYING STUFF */}
-              {/* {this.state.items ? this.state.items.map((item) => {
-              })} */}
+            </div>
+            <div className="container">
+              <div className="row">
+                {/* filter allows us to search by item name */}
+                {this.state.items
+                  .filter(
+                    (item) =>
+                      item.itemname
+                        .toLowerCase()
+                        .trim()
+                        .includes(this.state.search.toLowerCase().trim()) ||
+                      item.category
+                        .toLowerCase()
+                        .includes(this.state.search.toLowerCase())
+                  )
+                  .map((item) => {
+                    return (
+                      <div
+                        className="col-xsm-12 col-md-4 col-sm-12"
+                        key={item._id}
+                      >
+                        <div className="card item-card">
+                          <div className="row">
+                            <div className="col-4">
+                              <nav className="card-title">{item.itemname}</nav>
+
+                              <img
+                                src={item.image}
+                                className="card-img"
+                                alt={item.name}
+                              />
+                            </div>
+                            <br></br>
+                            <div className="content">
+                              <ul>
+                                <br></br>
+                                <li>
+                                  <strong>Condition:</strong> {item.condition}
+                                </li>
+                                <br></br>
+                                <li>
+                                  <strong>Current bid: $ </strong>
+                                  {item.startingbid}
+                                </li>
+                                <br></br>
+                                <li>
+                                  <strong>Buyout price: $ </strong>
+                                  {item.buyout}{" "}
+                                </li>
+                              </ul>
+
+                              <button
+                                className="btn btn-outline-secondary buy"
+                                onClick={() => this.handleBuyNow(item)}
+                              >
+                                Buy Now
+                              </button>
+                              <br></br>
+                              <br></br>
+
+                              <button
+                                className="btn btn-outline-secondary bid"
+                                type="text"
+                                onClick={(e) => this.handleBidSubmit(e, item)}
+                              >
+                                Place bid
+                              </button>
+                              <form>
+                                <div className="form-row">
+                                  <div className="form-group col-sm-8">
+                                    <input
+                                      id={item._id}
+                                      type="number"
+                                      className="form-control placeBid"
+                                      name={item._id}
+                                      value={this.state.highestbid[item._id]}
+                                      placeholder="Bid Here"
+                                      onChange={this.handleBidPriceInput}
+                                    />
+                                  </div>
+                                </div>
+                              </form>
+                            </div>
+                          </div>
+                        </div>
+                        <br></br>
+                      </div>
+                    );
+                  })}
+              </div>
             </div>
           </div>
         </div>
